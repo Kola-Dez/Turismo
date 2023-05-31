dd56ae2f1ca13115ab8b180fc1b45761d7022c30
vacban <kola.stalker.04@gmail.com>
2023-06-01 02:49:53 +0300

    debug

--- index.php
+++ index.php
@@ -2,21 +2,12 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="style/lol.css">
   <title>Turismo</title>
 </head>
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap');
-  *{
-      margin: 0;
-      padding: 0;
-      font-family: 'Poppins', sans-serif;
-
-  }
-</style>
 <body>
     <div class  ="head"><?php require_once 'attributes/head/head.php';?></div>
 
-    
     <div class="content"><?php require_once 'attributes/content/content.php';?></div>
 
     <div class="footer"><?php require_once 'attributes/footer/footer.php';?></div>
